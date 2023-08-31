d5d2d3df35c0a5036f94964b885fb010590d92a9
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-08-31 23:58:22 +0700

    add tailwindcss and redesign

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,37 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import Banner from '@/components/Banner'
-import KnowledgeCard from '@/components/KnowledgeCard'
+import HospitalCard from '@/components/HospitalCard'
+import Navbar from '@/components/Navbar';
 
 export default function Home() {
+  const hospitalData = [
+    {
+      name: 'Chulalongkorn Hospital',
+      imageSrc: '/CHULA-logo.png'
+    },
+    {
+      name: 'Rajavithi Hospital',
+      imageSrc: '/RAJAVITHI-logo.png'
+    },
+    {
+      name: 'Thammasat University Hospital',
+      imageSrc: '/THAMMASAT-logo.png'
+    }
+  ];
   return (
     <main >
+       <Navbar />
         <Banner/>
-       <KnowledgeCard />
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {hospitalData.map((hospital, index) => (
+        <HospitalCard 
+          key={index} 
+          name={hospital.name} 
+          imageSrc={hospital.imageSrc} 
+        />
+      ))}
+    </div>
     </main>
   )
 }
