992b584a151c613dfff8c54df17711bba50d6f48
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-09-14 19:24:57 +0700

    add interaction card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,28 @@
 import Image from "next/image";
-import { Prompt } from "next/font/google";
+import { Inter, Prompt } from "next/font/google";
 import Banner from "@/components/Banner";
 import HospitalCard from "@/components/HospitalCard";
 import Navbar from "@/components/Navbar";
-const prompt = Prompt({ subsets: ["latin"], weight: "400", style: "normal", variable: "--font-prompt" });
+import InteractionCard from "@/components/InteractionCard";
+const prompt = Prompt({
+  subsets: ["latin"],
+  weight: "400",
+  style: "normal",
+  variable: "--font-prompt",
+});
 export default function Home() {
   const hospitalData = [
     {
       name: "Chulalongkorn Hospital",
-      imageSrc: "/CHULA-logo.png",
+      imageSrc: "/chula.jpg",
     },
     {
       name: "Rajavithi Hospital",
-      imageSrc: "/RAJAVITHI-logo.png",
+      imageSrc: "/rajavithi.jpg",
     },
     {
       name: "Thammasat University Hospital",
-      imageSrc: "/THAMMASAT-logo.png",
+      imageSrc: "/thammasat.jpg",
     },
   ];
   return (
@@ -25,14 +31,19 @@ export default function Home() {
       <Banner />
 
       {/* Added centered text above the grid */}
-      <div className={`${prompt.variable} font-sans text-center text-black text-lg md:text-xl lg:text-2xl font-bold mt-8`}>
-        <h2>โรงพยาบาลที่เข้าร่วมโครงการ</h2>
+      <div
+        className={`${prompt.variable} font-sans text-center text-black text-lg md:text-xl lg:text-2xl font-bold mt-8`}
+      >
+        <h2>List of Hospitals participating in the project</h2>
       </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-4">
+      <div className="flex justify-center gap-4 mt-4">
         {hospitalData.map((hospital, index) => (
-          <div className="mt-4" key={index}>
-            <HospitalCard name={hospital.name} imageSrc={hospital.imageSrc} />
+          <div className="mt-4 w-72 h-96" key={index}>
+            <InteractionCard
+              serverComponent={<HospitalCard name={hospital.name} imageSrc={hospital.imageSrc} />}
+            />
+            
           </div>
         ))}
       </div>
