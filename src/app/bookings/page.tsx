16b8ec50a77c8d83282bab6af84ea62ed4864449
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-09-14 19:33:08 +0700

    change question to match requirement

--- src/app/bookings/page.tsx
+++ src/app/bookings/page.tsx
@@ -1,65 +1,76 @@
-"use client"; 
+'use client'
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker.css';
 
 const Booking = () => {
   const [formData, setFormData] = useState({
     name: '',
-    service: 'General Checkup',
+    nationalID: '',
+    hospital: 'Chulalongkorn Hospital',
     date: new Date(),
-    time: '09:00 AM',
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({
       ...formData,
       [name]: value,
     });
   };
-  
-  const handleDateChange = (date: Date) => {
+
+  const handleDateChange = (date) => {
     setFormData({
       ...formData,
       date,
     });
   };
-  
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+
+  const handleSubmit = (e) => {
     e.preventDefault();
     console.log('Form data submitted:', formData);
+    // You can add code here to send the form data to your server or perform other actions.
   };
-  
-
-
 
   return (
     <div className="container mx-auto mt-12">
-      <h1 className="text-2xl text-black font-semibold mb-4">Book an Appointment</h1>
+      <h1 className="text-2xl text-black font-semibold mb-4">Book a Vaccine Appointment</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">Your Name</label>
+          <label className="block text-sm font-medium text-gray-600">Full Name</label>
           <input
             type="text"
             name="name"
             value={formData.name}
             onChange={handleInputChange}
             className="mt-1 p-2 w-full border rounded-md"
+            required
           />
         </div>
 
         <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">Service</label>
+          <label className="block text-sm font-medium text-gray-600">National ID</label>
+          <input
+            type="text"
+            name="nationalID"
+            value={formData.nationalID}
+            onChange={handleInputChange}
+            className="mt-1 p-2 w-full border rounded-md"
+            required
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="block text-sm font-medium text-gray-600">Hospital</label>
           <select
-            name="service"
-            value={formData.service}
+            name="hospital"
+            value={formData.hospital}
             onChange={handleInputChange}
             className="mt-1 p-2 w-full border rounded-md"
           >
-            <option value="General Checkup">General Checkup</option>
-            <option value="Vaccination">Vaccination</option>
-            <option value="Consultation">Consultation</option>
+            <option value="Chulalongkorn Hospital">Chulalongkorn Hospital</option>
+            <option value="Rajavithi Hospital">Rajavithi Hospital</option>
+            <option value="Thammasat University Hospital">Thammasat University Hospital</option>
           </select>
         </div>
 
@@ -70,21 +81,6 @@ const Booking = () => {
             onChange={handleDateChange}
             className="mt-1 p-2 w-full border rounded-md"
           />
-        </div>
-
-        <div className="mb-4">
-          <label className="block text-sm font-medium text-gray-600">Time</label>
-          <select
-            name="time"
-            value={formData.time}
-            onChange={handleInputChange}
-            className="mt-1 p-2 w-full border rounded-md"
-          >
-            <option value="09:00 AM">09:00 AM</option>
-            <option value="10:00 AM">10:00 AM</option>
-            <option value="11:00 AM">11:00 AM</option>
-            <option value="12:00 PM">12:00 PM</option>
-          </select>
         </div>
 
         <button type="submit" className="py-2 px-4 bg-blue-500 text-white rounded">
