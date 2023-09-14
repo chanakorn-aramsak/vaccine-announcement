7e4da3e888dbbad3a65ba25c136ad690cb98363f
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-09-14 18:36:47 +0700

    fix bug on light theme

--- src/components/HospitalCard.tsx
+++ src/components/HospitalCard.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React from "react";
+import Image from "next/image";
 interface HospitalCardProps {
   name: string;
   imageSrc: string;
@@ -7,17 +7,20 @@ interface HospitalCardProps {
 
 const HospitalCard: React.FC<HospitalCardProps> = ({ name, imageSrc }) => {
   return (
-    <div className="flex flex-col items-center justify-between max-w-sm bg-white rounded-lg overflow-hidden shadow-md hover:shadow-lg transition duration-200 ease-in-out">
-      <div className="relative w-full h-48">
-        <img className="absolute w-full h-full object-cover" src={imageSrc} alt={name} />
+    <div className="max-w-md w-[1/3] h-[300px] rounded-lg shadow-lg ">
+      <div className="relative w-full h-[70%] rounded-t-lg ">
+        <Image
+          className="object-cover rounded-t-lg"
+          src={imageSrc}
+          fill={true}
+          alt={name}
+        />
       </div>
-      <div className="flex flex-col items-start justify-between w-full p-4">
-        <div className="font-semibold text-lg mb-2 text-black">{name}</div>
-      </div>
-      <div className="w-full p-4 flex justify-end">
+      <div className="w-full h-[30%] font-bold text-xl mb-2 text-black">
+        {name}
       </div>
     </div>
   );
-}
+};
 
 export default HospitalCard;
