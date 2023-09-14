9fbc634804569c58ca4ea77abe7eba1472037e8b
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-09-14 19:24:44 +0700

    change photo of hospital card

--- src/components/HospitalCard.tsx
+++ src/components/HospitalCard.tsx
@@ -4,21 +4,25 @@ interface HospitalCardProps {
   name: string;
   imageSrc: string;
 }
-
+const detail = "";
 const HospitalCard: React.FC<HospitalCardProps> = ({ name, imageSrc }) => {
   return (
-    <div className="max-w-md w-[1/3] h-[300px] rounded-lg shadow-lg ">
+    <div className="max-w-md w-[1/3] h-[300px] rounded-lg shadow-lg bg-white">
       <div className="relative w-full h-[70%] rounded-t-lg ">
         <Image
           className="object-cover rounded-t-lg"
           src={imageSrc}
           fill={true}
           alt={name}
+          layout="fill"
+          objectFit="contain"
         />
       </div>
-      <div className="w-full h-[30%] font-bold text-xl mb-2 text-black">
+      <div className="w-full h-[30%] font-bold text-xl mb-2 text-black text-center">
         {name}
       </div>
+
+      
     </div>
   );
 };
