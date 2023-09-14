31659ee0eed6c026a6e4dab08ae3ae69fee8fd16
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-09-14 21:03:20 +0700

    add CardPanel with reducer change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,21 @@
 import Image from "next/image";
-import { Inter, Prompt } from "next/font/google";
+
 import Banner from "@/components/Banner";
 import HospitalCard from "@/components/HospitalCard";
 import Navbar from "@/components/Navbar";
 import InteractionCard from "@/components/InteractionCard";
-const prompt = Prompt({
-  subsets: ["latin"],
-  weight: "400",
-  style: "normal",
-  variable: "--font-prompt",
-});
+import CardPanel from "@/components/CardPanel";
+
 export default function Home() {
-  const hospitalData = [
-    {
-      name: "Chulalongkorn Hospital",
-      imageSrc: "/chula.jpg",
-    },
-    {
-      name: "Rajavithi Hospital",
-      imageSrc: "/rajavithi.jpg",
-    },
-    {
-      name: "Thammasat University Hospital",
-      imageSrc: "/thammasat.jpg",
-    },
-  ];
+  
   return (
     <main className={`font-sans`}>
       <Navbar />
       <Banner />
+      <CardPanel />
+      
 
-      {/* Added centered text above the grid */}
-      <div
-        className={`${prompt.variable} font-sans text-center text-black text-lg md:text-xl lg:text-2xl font-bold mt-8`}
-      >
-        <h2>List of Hospitals participating in the project</h2>
-      </div>
-
-      <div className="flex justify-center gap-4 mt-4">
-        {hospitalData.map((hospital, index) => (
-          <div className="mt-4 w-72 h-96" key={index}>
-            <InteractionCard
-              serverComponent={<HospitalCard name={hospital.name} imageSrc={hospital.imageSrc} />}
-            />
-            
-          </div>
-        ))}
-      </div>
+      
     </main>
   );
 }
--- src/components/HospitalCard.tsx
+++ src/components/HospitalCard.tsx
@@ -1,11 +1,15 @@
-import React from "react";
+'use client'
+import React, {useState} from "react";
 import Image from "next/image";
+import { Rating } from "@mui/material";
+
 interface HospitalCardProps {
   name: string;
   imageSrc: string;
 }
-const detail = "";
+
 const HospitalCard: React.FC<HospitalCardProps> = ({ name, imageSrc }) => {
+  const [value, setValue] = useState<number | null>(0);
   return (
     <div className="max-w-md w-[1/3] h-[300px] rounded-lg shadow-lg bg-white">
       <div className="relative w-full h-[70%] rounded-t-lg ">
@@ -21,8 +25,15 @@ const HospitalCard: React.FC<HospitalCardProps> = ({ name, imageSrc }) => {
       <div className="w-full h-[30%] font-bold text-xl mb-2 text-black text-center">
         {name}
       </div>
-
-      
+      <div className="w-full h-[30%] font-bold text-xl mb-2 text-black text-center">
+        <Rating
+          name="simple-controlled"
+          value={value}
+          onChange={(event, newValue) => {
+            setValue(newValue);
+          }}
+        />
+        </div>
     </div>
   );
 };
