b9fc8439f2a8569ed715a7f0ebbbfa84209c8daa
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-04 14:10:56 +0700

    remove navbar(move to layout instead)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,6 @@
 'use client'
-import Image from "next/image";
 import Banner from "@/components/Banner";
-import HospitalCard from "@/components/HospitalCard";
 import Navbar from "@/components/Navbar";
-import InteractionCard from "@/components/InteractionCard";
 import CardPanel from "@/components/CardPanel";
 
 export default function Home() {
@@ -12,7 +9,7 @@ export default function Home() {
     <main className={`font-sans`}>
       <Navbar />
       <Banner />
-      <CardPanel />
+
     </main>
   );
 }
