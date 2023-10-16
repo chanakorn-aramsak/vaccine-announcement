42fb689e335d7c21246a83f8533d82b04383b6ea
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-16 14:17:15 +0700

    add google drive config on images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
+const nextConfig = {
+    images: {
+        domains: ['drive.google.com'],
+    },
+}
 module.exports = nextConfig
