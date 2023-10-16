809763b7c1e9a747d7cabefe015544de429221ed
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-16 14:17:04 +0700

    add linear progress

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,7 +1,18 @@
 'use client'
-import CardPanel from "@/components/CardPanel";
+import HospitalCatalog from '@/components/HospitalCatalog'
+import getHospitals from '@/libs/getHospitals'
+import { LinearProgress } from '@mui/material'
+import { Suspense } from 'react'
 
-const  HospitalCard = () => {
-  return <CardPanel />;
+const HospitalCard = () => {
+  const hospitals = getHospitals()
+  return (
+    <main className='mt-28'>
+      <Suspense fallback={<p>Loading... <LinearProgress /> </p>}>
+        <HospitalCatalog hospitalsResponse={hospitals} />
+      </Suspense>
+    </main>
+  )
 }
+
 export default HospitalCard;
