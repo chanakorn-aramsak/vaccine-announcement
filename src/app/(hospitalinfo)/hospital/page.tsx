76640c28f3a833e95568c57bddf76d2275715663
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-16 15:12:50 +0700

    refactor

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,18 +1,21 @@
-'use client'
-import HospitalCatalog from '@/components/HospitalCatalog'
-import getHospitals from '@/libs/getHospitals'
-import { LinearProgress } from '@mui/material'
-import { Suspense } from 'react'
+"use client";
+import HospitalCatalog from "@/components/HospitalCatalog";
+import { IHospitalsResponse } from "@/interfaces/IHospitalsResponse";
+import getHospitals from "@/libs/getHospitals";
+
+import { LinearProgress } from "@mui/material";
+import { Suspense, useEffect, useState } from "react";
 
 const HospitalCard = () => {
-  const hospitals = getHospitals()
+  const response = getHospitals();
+
   return (
-    <main className='mt-28'>
-      <Suspense fallback={<p>Loading... <LinearProgress /> </p>}>
-        <HospitalCatalog hospitalsResponse={hospitals} />
+    <main className="mt-28">
+      <Suspense fallback={<LinearProgress />}>
+        <HospitalCatalog hospitalsResponse={response} />
       </Suspense>
     </main>
-  )
-}
+  );
+};
 
 export default HospitalCard;
--- src/libs/getHospitals.tsx
+++ src/libs/getHospitals.tsx
@@ -1,16 +1,13 @@
-import { IHospitalsResponse } from "@/interfaces/IHospitalsResponse"
-
-
-
+import { IHospitalsResponse } from "@/interfaces/IHospitalsResponse";
 
 export default async function getHospitals(): Promise<IHospitalsResponse> {
-    // add timeout for loading delay testing
-    await new Promise(resolve => setTimeout(resolve, 5000))
+  // add timeout for loading delay testing
+  await new Promise((resolve) => setTimeout(resolve, 5000));
 
-    const response = await fetch('http://localhost:5000/api/v1/hospitals')
-    if (!response.ok) {
-        throw new Error("Something went wrong while fetching the hospitals data.")
-    }
+  const response = await fetch("http://localhost:5000/api/v1/hospitals");
+  if (!response.ok) {
+    throw new Error("Something went wrong while fetching the hospitals data.");
+  }
 
-    return await response.json()
+  return await response.json();
 }
