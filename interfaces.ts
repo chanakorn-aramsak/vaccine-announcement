bab8447332a0414071b1f5889a296c100d96aa10
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-10-31 10:23:45 +0700

    implement mybooking using redux

---
+++ interfaces.ts
@@ -0,0 +1,7 @@
+export interface BookingItem {
+    firstName: string;
+    lastName: string;
+    sid: string;
+    hospitalName: string;
+    date: string;
+}
---
+++ src/app/mybooking/page.tsx
@@ -0,0 +1,54 @@
+"use client"
+import { useDispatch } from "react-redux";
+import { AppDispatch, useAppSelector } from "@/redux/store";
+import { removeBooking } from "@/redux/features/bookSlice";
+
+export default function MyBooking() {
+  const dispatch = useDispatch<AppDispatch>();
+  const bookItem = useAppSelector((state) => state.bookSlice.bookItem);
+
+  if (
+    !bookItem.firstName &&
+    !bookItem.lastName &&
+    !bookItem.sid &&
+    !bookItem.hospitalName &&
+    !bookItem.date
+  ) {
+    return (
+      <div className="m-20">
+        <h1 className="text-2xl font-semibold text-gray-700">No Vaccine Booking</h1>
+      </div>
+    );
+  }
+
+  return (
+    <div className="m-20">
+      <h1 className="text-2xl font-semibold mb-10 text-gray-700">Your booking information</h1>
+
+      <div className="bg-blue-100 p-8 rounded shadow-lg mb-8">
+        <p className="mb-4">
+          <strong>Firstname:</strong> {bookItem.firstName}
+        </p>
+        <p className="mb-4">
+          <strong>Lastname:</strong> {bookItem.lastName}
+        </p>
+        <p className="mb-4">
+          <strong>SID:</strong> {bookItem.sid}
+        </p>
+        <p className="mb-4">
+          <strong>Hospital Name:</strong> {bookItem.hospitalName}
+        </p>
+        <p>
+          <strong>Date:</strong> {bookItem.date}
+        </p>
+      </div>
+
+      <button
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full transition duration-300 shadow-md"
+        onClick={() => dispatch(removeBooking(bookItem))}
+      >
+        Cancel Booking
+      </button>
+    </div>
+  );
+}
