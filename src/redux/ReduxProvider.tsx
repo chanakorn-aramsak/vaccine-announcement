5a038027e418523a797daca72d5d8e4d28e28202
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-02 13:50:28 +0700

    implement redux-persist

--- src/redux/ReduxProvider.tsx
+++ src/redux/ReduxProvider.tsx
@@ -1,11 +1,20 @@
-"use client"
-import { store } from "./store"
-import { Provider } from "react-redux"
+"use client";
+import { store } from "./store";
+import { Provider } from "react-redux";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
-export default function ReduxProvider({children}: {children: React.ReactNode}) {
-    return (
-        <Provider store={store}>
-            {children}
-        </Provider>
-    )
+export default function ReduxProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  let reduxPersistor = persistStore(store);
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={reduxPersistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
