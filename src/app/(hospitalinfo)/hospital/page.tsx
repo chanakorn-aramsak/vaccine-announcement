b58fe9128ae5d1eed6cd26749256e06023e4a8fd
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-02 13:51:01 +0700

    handle admin role

--- src/app/(hospitalinfo)/hospital/page.tsx
+++ src/app/(hospitalinfo)/hospital/page.tsx
@@ -1,18 +1,26 @@
-"use client";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import AddHospitalForm from "@/components/AddHospitalForm";
 import HospitalCatalog from "@/components/HospitalCatalog";
 import { IHospitalsResponse } from "@/interfaces/IHospitalsResponse";
 import getHospitals from "@/libs/getHospitals";
+import getUserProfile from "@/libs/getUserProfile";
 
 import { LinearProgress } from "@mui/material";
+import { getServerSession } from "next-auth";
 import { Suspense, useEffect, useState } from "react";
 
-const HospitalCard = () => {
+const HospitalCard = async () => {
   const response = getHospitals();
-
+  const session = await getServerSession(authOptions);
+  var profile = null;
+  if (session) {
+    profile = await getUserProfile(session.user.token);
+  }
   return (
     <main className="mt-28">
       <Suspense fallback={<LinearProgress />}>
         <HospitalCatalog hospitalsResponse={response} />
+        {profile?.data?.role === "admin" ? <AddHospitalForm /> : null}
       </Suspense>
     </main>
   );
