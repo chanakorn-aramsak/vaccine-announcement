38949042c43778e53557065793d14d2fba5ffc0e
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-14 14:02:58 +0700

    fix bug not use reducer

--- src/components/HospitalCatalog.tsx
+++ src/components/HospitalCatalog.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useState, useEffect } from "react";
+import { useReducer, useState } from "react";
 import HospitalCard from "./HospitalCard";
 import InteractionCard from "./InteractionCard";
 import { Prompt } from "next/font/google";
 import { IHospitalsResponse } from "@/interfaces/IHospitalsResponse";
-import { IHospital } from "@/interfaces/IHospital";
+
 const prompt = Prompt({
   subsets: ["latin"],
   weight: "400",
@@ -12,27 +12,45 @@ const prompt = Prompt({
   variable: "--font-prompt",
 });
 
+const reviewReducer = (
+  state: Map<string, number | null>,
+  action: { type: string; hospitalName: string; rating: number | null }
+) => {
+  if (action.rating == null) {
+    action.rating = 0;
+  }
+  switch (action.type) {
+    case "add":
+      const newStateAdd = new Map(state);
+      newStateAdd.set(action.hospitalName, action.rating);
+      return newStateAdd;
+    case "remove":
+      const newStateRemove = new Map(state);
+      newStateRemove.delete(action.hospitalName);
+      return newStateRemove;
+    default:
+      return state;
+  }
+};
+
 const HospitalCatalog = async ({
   hospitalsResponse,
 }: {
   hospitalsResponse: Promise<IHospitalsResponse>;
 }) => {
-  const [allReviews, dispatchReview] = useState(new Map());
-  const [isRatingClicked, setIsRatingClicked] = useState(false);
+  // const [isRatingClicked, setIsRatingClicked] = useState(false);
 
   const hospitalData = (await hospitalsResponse).data;
 
+  const [allReviews, dispatchReview] = useReducer(reviewReducer, new Map());
+
   const handleRatingChange = (newRating: number, hospitalName: string) => {
-    const newReviews = new Map(allReviews);
-    newReviews.set(hospitalName, newRating);
-    dispatchReview(newReviews);
-    setIsRatingClicked(true);
+    dispatchReview({ type: "add", hospitalName, rating: newRating });
+    // setIsRatingClicked(true);
   };
 
   const handleReviewRemove = (hospitalName: string) => {
-    const newReviews = new Map(allReviews);
-    newReviews.delete(hospitalName);
-    dispatchReview(newReviews);
+    dispatchReview({ type: "remove", hospitalName, rating: null });
   };
 
   return (
@@ -60,7 +78,7 @@ const HospitalCatalog = async ({
           </div>
         ))}
       </div>
-      {isRatingClicked && (
+      {(
         <div className="flex justify-center mt-4">
           <div className="shadow-lg rounded-lg p-4 ">
             <h1 className="text-center text-3xl font-bold text-black">
