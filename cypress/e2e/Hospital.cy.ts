1657157f4cc1909c4cb3a47dc9d13ccb54ced7d3
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-16 20:01:42 +0700

    complete testing assisgnment14 requirement

--- cypress/e2e/Hospital.cy.ts
+++ cypress/e2e/Hospital.cy.ts
@@ -1,9 +1,29 @@
-describe('template spec', () => {
-  it('should pause video and play', () => {
-    cy.visit("/")
+describe('Vaccine book app', () => {
+  it('should pause the video before play', () => {
+ // Visit the homepage
+ cy.visit("/");
 
-    cy.get('video').should('exist')
-    cy.get('button[id="play-button"]').should('contain', 'Pause');
-    cy.get('video').should('have.prop', 'paused', false);
+ // Check if the video exists and is visible
+ cy.get('video').should('exist').and('be.visible');
+
+ // Assert that the video is playing (paused property is false)
+ cy.get('video').should('have.prop', 'paused', false);
+
+ // Wait for 5 seconds
+ cy.wait(5000);
+
+ // Click the button to pause the video
+ cy.get('button[id="play-button"]').click();
+
+ // Assert that the video is paused (paused property is true)
+ cy.get('video').should('have.prop', 'paused', true);
+    
+
+  // click on the navigate /hospital button
+  cy.get('button[id="navigate-hospital-button"]').click();
+
+  // check if the hospital page contain 3 images
+  cy.get('img').should('have.length.at.least', 3);
   })
+  
 })
