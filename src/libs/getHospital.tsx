4babfec14f1dc1ddc7ab0895613858e43f8e621e
Chanakorn Aramsak <85776877+chanakorn-aramsak@users.noreply.github.com>
2023-11-25 21:33:09 +0700

    add env api

--- src/libs/getHospital.tsx
+++ src/libs/getHospital.tsx
@@ -1,7 +1,7 @@
 import { IHospitalResponse } from "@/interfaces/IHospitalResponse"
 
 export default async function getHospital(id: string): Promise<IHospitalResponse> {
-    const response = await fetch(`http://localhost:5000/api/v1/hospitals/${id}`,  {next: {tags: ['hospitals']}})
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/hospitals/${id}`,  {next: {tags: ['hospitals']}})
     if (!response.ok) {
         throw new Error("Something went wrong while fetching the hospital data.")
     }
--- src/libs/userLogIn.tsx
+++ src/libs/userLogIn.tsx
@@ -1,6 +1,6 @@
 const userLogIn = async (userEmail:string, userPassword:string) => {
   try {
-    const response = await fetch("http://localhost:5000/api/v1/auth/login", {
+    const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
